a67dde13923635802b2298f2f4cbdc37c63acb45
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-10-11 18:01:39 -0300

    continuando proj

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se ao executar getSavedCartItems o método localStorage.getItem é chamado', async () => {
+    const testCall = await getSavedCartItems(item);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  test('se ao executar getSavedCartItems, o método localStorage.getItem é chamado com o \'cartItems\' como parâmetro.', async () => {
+    const testCall = await saveCartItems(test);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,13 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', async () => {
+    const testCall = await saveCartItems(item);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  test('se ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems', async () => {
+    const test = 'Testando';
+    const testCall = await saveCartItems(test);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', test);
+  });
 });
